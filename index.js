f58455b3fe52b87cee882cba8427a3c4fd765873
Ripon <habibkazi92@gmail.com>
2021-02-06 12:26:53 +0800

    done

--- index.js
+++ index.js
@@ -1,39 +1,74 @@
 fetch(`https://restcountries.eu/rest/v2/all`)
     .then(response => response.json())
     .then(data => displayCountries(data))
-
+    
 const displayCountries = countries => {
     const countriesDiv = document.getElementById("countries");
-    console.log(countries);
-
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
-
+    
+    countries.forEach(country => {
         const countryDiv = document.createElement("div");
+        
         countryDiv.className = "country";
-
-        // const name = document.createElement("h3");
-        // name .innerHTML = country.name;
-        // const capital = document.createElement("p");
-        // capital.innerHTML = country.capital;
-
-        // const area = document.createElement("h6");
-        // area.innerHTML = country.area;
-        
-        // countryDiv.appendChild(name);
-        // countryDiv.appendChild(capital);
-        // countryDiv.appendChild(area);
-        
         const countryInfo = `
-             <h3 class="country-name">${country.name}</h3>
-             <p class="country-capital">${country.capital}</p>
-             <h6 class="country-population">${country.population}</h6>
-             <button class="btn btn-success Details"> Details</button>
-             
+        <h3 class="country-name">${country.name} </h3>
+        <p class="country-capital">${country.capital}</p>
+        <button onclick="displayDetails('${country.name}')" class="btn btn-success Details"> Details</button>
         `
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
+
+        
+    });
+
+
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+
+    //     const countryDiv = document.createElement("div");
+    //     countryDiv.className = "country";
+
+    //     // const name = document.createElement("h3");
+    //     // name .innerHTML = country.name;
+    //     // const capital = document.createElement("p");
+    //     // capital.innerHTML = country.capital;
+
+    //     // const area = document.createElement("h6");
+    //     // area.innerHTML = country.area;
+        
+    //     // countryDiv.appendChild(name);
+    //     // countryDiv.appendChild(capital);
+    //     // countryDiv.appendChild(area);
+        
+    //     const countryInfo = `
+    //          <h3 class="country-name">${country.name}</h3>
+    //          <p class="country-capital">${country.capital}</p>
+    //          <h6 class="country-population">${country.population}</h6>
+    //          <button class="btn btn-success Details"> Details</button>
+             
+    //     `
+    //     countryDiv.innerHTML = countryInfo;
+    //     countriesDiv.appendChild(countryDiv);
        
-    }
+    // }
+
+}
+
+const displayDetails = name =>{
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data=> renderCountryInfo(data[0]));
+    
+
+
+}
+const renderCountryInfo = country=>{
+    console.log(country);
+    const countryDiv = document.getElementById("countryDetails");
+    countryDiv.innerHTML = `
+    <h1> ${country.name}</h1>
+    <h6 >${country.region}</h6>
+    <img src="${country.flag}">
+    `
 
 }
