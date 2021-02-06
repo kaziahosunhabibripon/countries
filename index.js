5f6080ec14769d95bed3e3b4684acff18dba27be
Ripon <habibkazi92@gmail.com>
2021-02-06 10:30:48 +0800

    simple for loop

--- index.js
+++ index.js
@@ -5,23 +5,35 @@ fetch(`https://restcountries.eu/rest/v2/all`)
 const displayCountries = countries => {
     const countriesDiv = document.getElementById("countries");
     console.log(countries);
+
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
 
         const countryDiv = document.createElement("div");
+        countryDiv.className = "country";
 
-        const name = document.createElement("h3");
-        name .innerHTML = country.name;
-        const capital = document.createElement("p");
-        capital.innerHTML = country.capital;
+        // const name = document.createElement("h3");
+        // name .innerHTML = country.name;
+        // const capital = document.createElement("p");
+        // capital.innerHTML = country.capital;
 
-        const area = document.createElement("h6");
-        area.innerHTML = country.area;
+        // const area = document.createElement("h6");
+        // area.innerHTML = country.area;
+        
+        // countryDiv.appendChild(name);
+        // countryDiv.appendChild(capital);
+        // countryDiv.appendChild(area);
+        
+        const countryInfo = `
+             <h3 class="country-name">${country.name}</h3>
+             <p class="country-capital">${country.capital}</p>
+             <h6 class="country-population">${country.population}</h6>
+             <button class="btn btn-success Details"> Details</button>
+             
+        `
+        countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
-        countryDiv.appendChild(name);
-        countryDiv.appendChild(capital);
-        countryDiv.appendChild(area);
-        // console.log(country.name);
+       
     }
 
 }
