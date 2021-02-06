09edc3ac4aef63e4635f29e42d28257485c06533
Ripon <habibkazi92@gmail.com>
2021-02-06 09:56:22 +0800

    simple way

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ fetch(`https://restcountries.eu/rest/v2/all`)
     .then(data => displayCountries(data))
 
 const displayCountries = countries => {
-    const countryDiv = document.getElementById("countries");
+    const countriesDiv = document.getElementById("countries");
     console.log(countries);
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
-        console.log(country.name);
 
+        const countryDiv = document.createElement("div");
+
+        const name = document.createElement("h3");
+        name .innerHTML = country.name;
+        const capital = document.createElement("p");
+        capital.innerHTML = country.capital;
+
+        const area = document.createElement("h6");
+        area.innerHTML = country.area;
+        countriesDiv.appendChild(countryDiv);
+        countryDiv.appendChild(name);
+        countryDiv.appendChild(capital);
+        countryDiv.appendChild(area);
+        // console.log(country.name);
     }
 
 }
